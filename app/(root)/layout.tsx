ebe711af2de9da0a0e1c4ab56f97e1c7f4d68218
rainingchicken <rainwhenever@gmail.com>
2025-01-08 17:51:11 -0800

    home and rightsidebar styling and placeholder

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 
 export default function RootLayout({
@@ -10,17 +12,24 @@ export default function RootLayout({
     <div className="relative flex flex-col">
       <main className="relative flex bg-black-3">
         <LeftSidebar />
-        <section>
-          <div>
-            <div>
-              <Image />
-              Mobile Nav
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px:14">
+          <div className="mx:auto flex w-full max-w-5xl flex-col max:sm: px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src={"/icons/lgo.svg"}
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
+              <MobileNav />
             </div>
-            <div>Toaster (navigatipn popup) {children} </div>
+            <div className="flex flex-col md:pb-14 ">
+              {" "}
+              Toaster (navigatipn popup) {children}{" "}
+            </div>
           </div>
         </section>
-
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+        <RightSidebar />
       </main>
     </div>
   );
