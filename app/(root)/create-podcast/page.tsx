1b0077b5d8cb908c9e1688a65e33aecbe2e4c9f0
rainingchicken <rainwhenever@gmail.com>
2025-01-08 22:13:27 -0800

    podcast name form, select ai voice in create-podcast

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,11 +1,121 @@
-import React from "react";
+"use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+const voiceCategories = ["alloy", "shimmer", "nova", "echo", "fable", "onyx"];
 const CreatePodcast = () => {
+  const [voiceType, setVoiceType] = useState<string | null>(null);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
-    <div className="">
+    <section className="mt-10 flex flex-col ">
       <h1 className="text-20 font-bold text-white-1">Create Podcast</h1>
-    </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-10 flex flex-col w-full"
+        >
+          <div className="flex flex-col gap-[30px] border-b border-black-5 pb-10">
+            <FormField
+              control={form.control}
+              name="podcastTitle"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2.5">
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Username
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="input-class focus-visible:ring-orange-1"
+                      placeholder="Interesting podcast"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-white-1" />
+                </FormItem>
+              )}
+            />
+            <div className="flex flex-col gap-2.5">
+              <Label className="text-16 text-white-1 font-bold">
+                Select AI voice
+              </Label>
+              <Select onValueChange={(value) => setVoiceType(value)}>
+                <SelectTrigger
+                  className={cn(
+                    "text-16 w-full border-none bg-black-1 text-gray-1"
+                  )}
+                >
+                  <SelectValue
+                    placeholder="Select AI voice"
+                    className="placeholder:text-gray-1"
+                  />
+                </SelectTrigger>
+                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1">
+                  {voiceCategories.map((category) => (
+                    <SelectItem
+                      key={category}
+                      value={category}
+                      className="capitalize bg-orange-1"
+                    >
+                      {category}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+                {voiceType && (
+                  <audio src={`${voiceType}.mp3`} autoPlay className="hidden" />
+                )}
+              </Select>
+            </div>
+          </div>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </section>
   );
 };
-
 export default CreatePodcast;
--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,10 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect();
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) auth.protect();
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
