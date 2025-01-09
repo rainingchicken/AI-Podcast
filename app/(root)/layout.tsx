f5d2c07aaa0a6dd1e5d5c03458ed648cf67a5013
rainingchicken <rainwhenever@gmail.com>
2025-01-08 17:36:58 -0800

    left side bar, navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,25 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
-        <p className="text-white-1">LEFT SIDEBAR</p>
-        {children} {/*MAIN*/}
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+        <section>
+          <div>
+            <div>
+              <Image />
+              Mobile Nav
+            </div>
+            <div>Toaster (navigatipn popup) {children} </div>
+          </div>
+        </section>
+
         <p className="text-white-1">RIGHT SIDEBAR</p>
       </main>
     </div>
