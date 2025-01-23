e5fc7c5bbee28356c59f41e6b418e65865780b9b
rainingchicken <rainwhenever@gmail.com>
2025-01-22 23:41:13 -0800

    rightsidebar profile link

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,37 @@
+"use client";
+import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Header from "./Header";
+import Carousel from "./Carousel";
 
 const RightSidebar = () => {
-  return <section className="right_sidebar text-white-1">RightSidebar</section>;
+  const { user } = useUser();
+  console.log(user);
+  return (
+    <section className="right_sidebar text-white-1">
+      <SignedIn>
+        <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
+          <UserButton />
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-16 truncate font-semibold text-white-1">
+              {user?.firstName} {user?.lastName}
+            </h1>
+            <Image
+              src={"/icons/right-arrow.svg"}
+              width={24}
+              height={24}
+              alt="arrow"
+            />
+          </div>
+        </Link>
+      </SignedIn>
+      <section>
+        <Header /> <Carousel />
+      </section>
+    </section>
+  );
 };
 
 export default RightSidebar;
